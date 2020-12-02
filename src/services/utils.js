f1f327c0e269d15aad3f3497e1b911bc104f594b
mac <mac@macs-MacBook-Pro.local>
2020-12-02 10:46:56 +0100

    Set up Redux Saga Add services utils (basic fetch)

---
+++ src/services/utils.js
@@ -0,0 +1,27 @@
+//api
+export const api = 'http://34.76.140.183'
+
+export const realtors = '/realtors/'
+export const messages = '/messages/'
+
+//BASIC FETCH API METHOD
+export const basicFetch = async (method, url, config, data) => {
+  try {
+    const response = await fetch(url, {
+      method: method,
+      headers: {
+        Accept: '*/*',
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: data
+    })
+    const textResponse = await response.text()
+    const result = {
+      data: textResponse,
+      status: response.status
+    }
+    return result
+  } catch (error) {
+    return error
+  }
+}
