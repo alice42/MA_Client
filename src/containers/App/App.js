b6d761061575831614e590574a55136e69a0dda4
mac <mac@macs-MacBook-Pro.local>
2020-12-02 11:35:02 +0100

    one simple route

---
+++ src/containers/App/App.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { connect } from 'react-redux'
+import { bindActionCreators } from 'redux'
+import * as dataActions from '../../actions/dataActions'
+
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom'
+
+import Layout from '../../components/Layout'
+import Home from '../Home'
+
+const App = props => (
+  <BrowserRouter basename="/">
+    <LayoutConnected>
+      <Switch>
+        <Route exact path={'/'} component={HomeConnected} />
+        <Redirect to="/" />
+      </Switch>
+    </LayoutConnected>
+  </BrowserRouter>
+)
+
+const actionsMapDispatchToProps = dispatch => {
+  return {
+    dataActions: bindActionCreators(dataActions, dispatch)
+  }
+}
+
+const mapStateToProps = state => {
+  const { data } = state
+  return {
+    data
+  }
+}
+
+const HomeConnected = connect(mapStateToProps, actionsMapDispatchToProps)(Home)
+const LayoutConnected = connect(
+  mapStateToProps,
+  actionsMapDispatchToProps
+)(Layout)
+
+export default connect(mapStateToProps, actionsMapDispatchToProps)(App)
